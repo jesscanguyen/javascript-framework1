fab9f27f08da41375eb529871618932536b27d5c
jesscanguyen <126618898+jesscanguyen@users.noreply.github.com>
2025-02-23 06:35:35 +0000

    put in alpine.js, starting to create random name generator

--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,11 @@
-alert( 'Hello, world!' );
+function clicked() {
+    alert('haiii!');
+}
+
+window.onload = function() {
+    const click = document.getElementById('click'); // set this variable to the document object that is identified by the id "click," lets you start an event listener //
+
+    click.addEventListener("click", (event) => {
+        clicked();
+    }); // basically means when your button with the ID of 'click' has a "click" event happen to it, it calls the function //
+}
